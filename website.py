3b786102dd1b7520acaa01bb074f1a93f4b4be65
Clement <clement.dupic@insa-lyon.fr>
2024-05-20 20:35:41 +0200

    update flask : moyen de se connecter et d'enregistrer la connexion

--- website.py
+++ website.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, session
-from lecture_SQL import SQL_lecture
+from flask import Flask, render_template, request, url_for, session, redirect
+#from lecture_SQL import SQL_lecture
 import requests
 import json
 import time
@@ -15,25 +15,29 @@ def index():
 def serres():
 	return render_template('getserres.html')
 
-@app.route('/traitement', methods=['POST'])
-def traitement():
-	if request.method == 'POST':
-		serre = request.form.get('serre')
-		sql = SQL_lecture()
-		mesures = sql.afficher_mesures_serre(serre)
-		print(mesures)
-		if not mesures :
-			#mesures = ["Aucune mesure pour cette serre"]
-			return redirect('/getserres')
-		else :
-			return render_template('mesures.html', mesures=mesures)
+@app.route('/test')
+def test():
+	return render_template('page_utilisateur.html')
 
-@app.route('/mesures')
-def mesures():
-	sql = SQL_lecture()
-	mesures = sql.afficher_mesures_serre("serre1")
-
-	return render_template('mesures.html', mesures=mesures)
+# @app.route('/traitement', methods=['POST'])
+# def traitement():
+# 	if request.method == 'POST':
+# 		serre = request.form.get('serre')
+# 		sql = SQL_lecture()
+# 		mesures = sql.afficher_mesures_serre(serre)
+# 		print(mesures)
+# 		if not mesures :
+# 			#mesures = ["Aucune mesure pour cette serre"]
+# 			return redirect('/getserres')
+# 		else :
+# 			return render_template('mesures.html', mesures=mesures)
+#
+# @app.route('/mesures')
+# def mesures():
+# 	sql = SQL_lecture()
+# 	mesures = sql.afficher_mesures_serre("serre1")
+#
+# 	return render_template('mesures.html', mesures=mesures)
 
 @app.route('/compteur')
 
@@ -45,6 +49,58 @@ def compteur():
 		session["compteur"] += 1
 	return f"Nombre de visites : {session['compteur']}"
 
+utilisateurs = [
+	{"nom" : "test",
+	 "mdp" : "test"}
+]
+
+def recherche_utilisateur(nom, mdp):
+	for utilisateur in utilisateurs:
+		if utilisateur['nom']==nom and utilisateur['mdp']==mdp:
+			print('valide')
+			return utilisateur
+	return None
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+	print(session)
+	if request.method == "POST":
+		donnees = request.form
+		nom = donnees.get('nom')
+		mdp = donnees.get('mdp')
+		utilisateur = recherche_utilisateur(nom, mdp)
+
+		if utilisateur is not None:
+			session['nom_utilisateur'] = utilisateur['nom']
+			return redirect(url_for('index'))
+		else:
+			return redirect(url_for('login'))
+	else: #importer car on peut accéder directement à la page depuis l'url donc risque de bypass
+		if 'nom_utilisateur' in session:
+			return redirect(url_for('index'))
+		else:
+			return render_template("login.html")
+
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+	session.pop('nom_utilisateur', None)
+	return redirect(url_for('index'))
+
+
+
+@app.route("/traitement", methods=["POST", "GET"])
+def traitement():
+	if request.method == "POST":
+		donnees = request.form
+		nom = donnees.get('nom')
+		mdp = donnees.get('mdp')
+		if nom == 'test' and mdp == 'test':
+			return render_template("traitement.html", nom_utilisateur=nom)
+		else:
+			return render_template("traitement.html")
+	else:
+		return redirect('/index')
 if __name__ == '__main__':
 	app.run(debug=True)
 
