5417f09bbbb9e0968cd671275afd48df0aaf4c15
Clement <clement.dupic@insa-lyon.fr>
2024-05-22 21:29:08 +0200

    modification des scripts sql

--- SQL_Script/up.sql
+++ SQL_Script/up.sql
@@ -9,13 +9,19 @@ CREATE TABLE Plants
     O2            FLOAT  # in %
 );
 
+CREATE TABLE Users
+(
+    pseudo VARCHAR(16) PRIMARY KEY,
+    password VARCHAR(32),
+    date_registration DATETIME DEFAULT NOW()
+);
 
 CREATE TABLE GreenHouses
 (
+    serial_number   VARCHAR(32) PRIMARY KEY,
     name            VARCHAR(16),
-    num_serie            VARCHAR(32) PRIMARY KEY,
     update_interval INT UNSIGNED,   # in seconds
-    plant_init_date DATETIME,
+    plant_init_date DATETIME DEFAULT NOW(),
     temperature     FLOAT, # in °C
     soil_humidity   INT,   # in %
     air_humidity    INT,   # in %
@@ -25,35 +31,26 @@ CREATE TABLE GreenHouses
     FOREIGN KEY (pseudo) REFERENCES Users (pseudo)
 );
 
-CREATE TABLE Users
-(
-    pseudo VARCHAR(16) PRIMARY KEY,
-    password VARCHAR(32),
-    date_registration DATETIME DEFAULT NOW()
-);
-
 CREATE TABLE GreenHousePlants
 (
     id              INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     plant_id        INT UNSIGNED,
-    greenhouse_name VARCHAR(16),
-    date_debut DATETIME,
+    serial_number   VARCHAR(32),
+    date_debut DATETIME DEFAULT NOW(),
     date_fin DATETIME DEFAULT NULL,
     FOREIGN KEY (plant_id) REFERENCES Plants (id),
-    FOREIGN KEY (greenhouse_name) REFERENCES GreenHouses (name)
+    FOREIGN KEY (serial_number) REFERENCES GreenHouses (serial_number)
 );
-
-
 
 
 CREATE TABLE Sensors
 (
     id              TINYINT UNSIGNED,
-    greenhouse_name VARCHAR(16),
+    serial_number   VARCHAR(32),
     type            ENUM ('temperature', 'soil_humidity', 'air_humidity', 'light', 'O2', 'water_level'),
     unit            VARCHAR(16),
-    PRIMARY KEY (id, greenhouse_name),
-    FOREIGN KEY (greenhouse_name) REFERENCES GreenHouses (name)
+    PRIMARY KEY (id, serial_number),
+    FOREIGN KEY (serial_number) REFERENCES GreenHouses (serial_number)
 );
 
 
@@ -70,17 +67,17 @@ CREATE TABLE Measures
 CREATE TABLE Actuators
 (
     id              TINYINT UNSIGNED,
-    greenhouse_name VARCHAR(16),
+    serial_number   VARCHAR(32),
     type            ENUM ('temperature', 'soil_humidity', 'air_humidity', 'light', 'O2', 'water_level'),
-    FOREIGN KEY (greenhouse_name) REFERENCES GreenHouses (name),
-    PRIMARY KEY (id, greenhouse_name)
+    FOREIGN KEY (serial_number) REFERENCES GreenHouses (serial_number),
+    PRIMARY KEY (id, serial_number)
 );
 
 
 CREATE TABLE Actions
 (
     actuator_id TINYINT UNSIGNED,
-    date        DATETIME,
+    date        DATETIME DEFAULT NOW(),
     value       FLOAT,
     FOREIGN KEY (actuator_id) REFERENCES Actuators (id),
     PRIMARY KEY (actuator_id, date)
