20c81f3fd6c572ba05e66793ae41984c8c66f80c
Clement <clement.dupic@insa-lyon.fr>
2024-05-24 11:33:54 +0200

    Todo résolus dans sql_utils.py et ajout de sql_connexion_test.py pour tester ces jointures sql dans le terminal en l'absence de test via interface web

---
+++ src/database/sql_connexion_test.py
@@ -0,0 +1,63 @@
+import mysql.connector as mysql
+from datetime import datetime
+
+#remarque globale : avec l'autoincrement, quand une mesure est supprimée, l'idMesure n'est pas réutilisé, il est incrémenté
+
+class SQL_connection:
+
+	def __init__(self):
+		self.connexion = None
+		self.cursor = None
+		self.ouvrir_connexion_bd()
+
+	def ouvrir_connexion_bd(self):
+		"""Ouvre une connexion à la base de données"""
+
+		print("")
+		print("***********************")
+		print("** Connexion à la BD **")
+		print("***********************")
+
+		try:
+			self.connexion = mysql.connect(
+				host="fimi-bd-srv1.insa-lyon.fr",
+				port=3306,
+				user="G221_A",      # à compléter
+				password="G221_A",  # à compléter
+				database="G221_A_BD3"   # à compléter
+			)
+			self.cursor = self.connexion.cursor()
+
+		except Exception as e:
+			if type(e) == NameError and str(e).startswith("name 'mysql'"):
+				print("[ERROR] MySQL: Driver 'mysql' not installed ? (Python Exception: " + str(e) + ")")
+				print("[ERROR] MySQL:" +
+					  " Package MySQL Connector should be installed [Terminal >> pip install mysql-connector-python ]" +
+					  " and imported in the script [import mysql.connector as mysql]")
+			else:
+				print("[ERROR] MySQL: " + str(e))
+
+		if self.connexion is not None:
+			print("=> Connexion établie...")
+		else:
+			print("=> Connexion échouée...")
+
+		return self.connexion
+
+	def fermer_connexion_bd(self):
+		"""Ferme la connexion à la base de données"""
+
+		if self.connexion is not None:
+			self.connexion.close()
+			print("=> Connexion fermée...")
+		else:
+			print("=> Connexion inexistante...")
+
+
+
+
+
+
+
+
+
--- src/database/sql_utils.py
+++ src/database/sql_utils.py
@@ -1,37 +1,65 @@
-from flask import g
+#from flask import g
+from sql_connexion_test import SQL_connection
+from datetime import datetime
 
 
-def get_greenhouse_measures(greenhouse_id):
-    cursor = g.db.connect().cursor()
+def get_greenhouse_measures(greenhouse_id, sensor_id, date_debut, date_fin):
+    # cursor = g.db.connect().cursor()
 
-    # TODO: parametrize the function to take in parameter the sensor_id and a date range
+    # j'utilise ici sql_connection pour me connecter à la base de données parce que g marche pas tant que le site est pas actif
+    sql_connection = SQL_connection()
+    cursor = sql_connection.cursor
+
     measures = {"temperature": [], "soil_humidity": [], "light": [], "air_humidity": [], "O2": [],
                    "water_level": []}
     try:
+
         cursor.execute(
-            "SELECT Measures.sensor_id, Measures.date, Measures.value, Sensors.type, Sensors.unit FROM Measures, "
-            "Sensors WHERE Measures.sensor_id = Sensors.id and Sensors.greenhouse_name= %s",
-            greenhouse_id)
+            "SELECT Measures.sensor_id, Measures.date, Measures.value, Sensors.type, Sensors.unit "
+            "FROM Measures, Sensors"
+            " WHERE Measures.sensor_id = Sensors.id  and Sensors.id = %s and Sensors.serial_number= %s and Measures.date BETWEEN %s and %s ",
+            (sensor_id, greenhouse_id, date_debut, date_fin))
+        
         for (sensor_id, date, value, type, unit) in cursor:
+            print(f"Mesure dans {greenhouse_id} le {date}, faite par le capteur {sensor_id}, {type} de {value} {unit}")
             measures[type].append([value, date, unit])
+
+        sql_connection.fermer_connexion_bd()
 
         return measures
 
     except Exception as e:
         print(f"Erreur lors de l'affichage des mesures de la serre {greenhouse_id}: {e}")
 
-def print_greenhouse_actions(greenhouse_id):
-    cursor = g.db.connect().cursor()
+def print_greenhouse_actions(greenhouse_id, actuator_id, date_debut, date_fin):
+    #cursor = g.db.connect().cursor()
 
-    # TODO: parametrize the function to take in parameter the actuator_id and a date range
+    # j'utilise ici sql_connection pour me connecter à la base de données parce que g marche pas tant que le site est pas actif
+    sql_connection = SQL_connection()
+    cursor = sql_connection.cursor
+
     try:
         cursor.execute(
             "SELECT Actions.actuator_id, Actions.date, Actions.value, Actuators.type FROM Actions, Actuators "
-            "WHERE Actions.actuator_id = Actuators.id and Actuators.greenhouse_name= %s",
-            greenhouse_id)
+            "WHERE Actions.actuator_id = Actuators.id and Actuators.id= %s and Actuators.serial_number= %s and Actions.date BETWEEN %s and %s",
+            (actuator_id, greenhouse_id, date_debut, date_fin))
 
         for (actuator_id, date, value, type) in cursor:
             print(f"Action dans {greenhouse_id} le {date}, faite par l'actionneur {actuator_id}, {type} de {value}")
 
+        sql_connection.fermer_connexion_bd()
+
     except Exception as e:
         print(f"Erreur lors de l'affichage des actions de la serre {greenhouse_id}: {e}")
+
+
+if __name__ == "__main__":
+    # Example of use
+    greenhouse_id = "test_serial"
+    sensor_id = "1"
+    actuator_id = "1"
+    date_debut = datetime(2024, 1, 1)
+    date_fin = datetime(2025, 1, 1)
+    measures = get_greenhouse_measures(greenhouse_id, sensor_id, date_debut, date_fin)
+    print(measures)
+    print_greenhouse_actions(greenhouse_id, actuator_id, date_debut, date_fin)
