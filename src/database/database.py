f3e50d6791deb925cd7a5b78a647306227a9e0a9
Clement <clement.dupic@insa-lyon.fr>
2024-05-26 09:21:00 +0200

    new method in database.py to add a mix of plants in a greenhouse

--- src/database/database.py
+++ src/database/database.py
@@ -221,3 +221,104 @@ def get_actuator_type(actuator_id):
     except Exception as e:
         print(f"Error when getting actuator type: {e}")
         return None
+
+
+def get_greenhouse_name(serial_number):
+    db = get_db()
+    cursor = db.cursor()
+
+    try:
+        cursor.execute(
+            "SELECT name "
+            "FROM GreenHouses "
+            "WHERE serial = %s",
+            (serial_number,)
+        )
+        name = cursor.fetchone()
+        if name is None:
+            return None
+        return name[0]
+
+    except Exception as e:
+        print(f"Error when getting greenhouse name: {e}")
+        return None
+
+
+def add_mix_plant(greenhouse_serial, list_plants):
+    db = get_db()
+    cursor = db.cursor()
+    list_plants_id = list_plants[0]
+    list_plants_units = list_plants[1]
+
+    try:
+
+        temperature_moy = soil_humidity_moy = air_humidity_moy = light_moy = O2_moy = 0
+        nb_plants = sum(list_plants_units)
+        serial_exist = False
+
+        cursor.execute("SELECT serial FROM GreenHouses WHERE EXISTS (SELECT serial FROM GreenHouses WHERE serial = %s )"
+                       , (greenhouse_serial,))
+        for (serial,) in cursor:
+            if serial:
+                serial_exist = True
+
+
+# get the average of the plants' parameters
+        for i in range(len(list_plants_id)):
+            cursor.execute("SELECT temperature, soil_humidity, air_humidity, light, O2 FROM Plants WHERE id = %s",
+                           (list_plants_id[i],))
+            for (temperature, soil_humidity, air_humidity, light, O2,) in cursor:
+                temperature_moy += temperature*list_plants_units[i]
+                soil_humidity_moy += soil_humidity*list_plants_units[i]
+                air_humidity_moy += air_humidity*list_plants_units[i]
+                light_moy += light*list_plants_units[i]
+                O2_moy += O2*list_plants_units[i]
+
+# check if the greenhouse already exists, if it doesn't, should show an error on the website
+        if serial_exist:
+            cursor.execute("UPDATE GreenHouses "
+                           "SET temperature = %s, soil_humidity = %s, air_humidity = %s, light = %s, O2 = %s, "
+                           "plant_init_date = NOW() "
+                           "WHERE serial = %s ",
+                            (temperature_moy/nb_plants, soil_humidity_moy/nb_plants, air_humidity_moy/nb_plants,
+                             light_moy/nb_plants, O2_moy/nb_plants, greenhouse_serial))
+            db.commit()
+
+# add the relations in GreenHousePlants and update them if they already exist
+        for plant_id in list_plants_id:
+
+            plant_exist = False
+            cursor.execute("SELECT plant_id FROM GreenHousePlants WHERE EXISTS (SELECT plant_id FROM GreenHouses WHERE "
+                           "plant_id = %s and greenhouse_serial = %s )"
+                           , (plant_id, greenhouse_serial,))
+            for (id_plant,) in cursor:
+                if id_plant:
+                    plant_exist = True
+
+            if not plant_exist:
+                cursor.execute("INSERT INTO GreenHousePlants (plant_id, greenhouse_serial) VALUES (%s , %s)",
+                    (plant_id, greenhouse_serial))
+                db.commit()
+
+            else:
+                cursor.execute("UPDATE GreenHousePlants SET date_start = NOW() "
+                               "WHERE plant_id = %s and greenhouse_serial = %s ",
+                    (plant_id, greenhouse_serial))
+                db.commit()
+
+
+# check if there are plants that are not in the list anymore :-> update the date_fin
+        cursor.execute("SELECT plant_id, date_end "
+                        "FROM GreenHousePlants WHERE greenhouse_serial = %s", (greenhouse_serial,))
+        for (plant_id, date_fin) in cursor:
+            if plant_id not in list_plants_id and date_fin is None:
+                cursor.execute("UPDATE GreenHousePlants SET date_end = NOW() "
+                               "WHERE plant_id = %s and greenhouse_serial = %s ",
+                        (plant_id, greenhouse_serial))
+                db.commit()
+
+    except Exception as e:
+        print(f"Error when adding plants: {e}")
+        return False
+
+
