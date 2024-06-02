54b5ec1cf91001b886f3625b8bb1ac9ae75581e9
Clément Grennerat <clement.grennerat@free.fr>
2024-06-02 21:39:24 +0200

    Updating form design + plant manager specification

--- src/managers/plant.py
+++ src/managers/plant.py
@@ -1,14 +1,14 @@
-from flask import session, redirect, url_for, request, render_template
-
-
 def plant_manager():
     # TODO:
-    #       If the parameter 'is_custom' is True,
+    #       If the parameter 'is-custom' is True,
     #           the config is specified as post parameters, e.g. 'temperature' = 25
     #           the code should update the greenhouse in the GreenHouses table and switch is_custom_config to true.
-    #       If the parameter 'is_custom' is False,
-    #           the new plants are specified in 'new_plants' as a comma separated list of plant ids.
-    #           the plants to delete are specified in 'remove_plants' as a comma separated list
-    #               of ASSOCIATION ids (id of GreenHousePlants).
+    #       If the parameter 'is-custom' is False,
+    #           - the new plants are specified in 'add-plants' as a comma separated list of plant ids.
+    #           - the amount of each plant is specified in 'add-plants-count', as a comma separated list of integers
+    #             (in the same order ar add-plants).
+    #           - the associations to delete are specified in 'remove-associations' as a comma separated list
+    #               of ASSOCIATION ids (id of GreenHousePlants). A value can appear multiple times.
+    #               If the value appears once while the association contains multiple plants, only one plant is removed.
     #       the code should update the plants in the GreenHousePlants table and switch is_custom_config to false.
     pass
--- static/js/plants.js
+++ static/js/plants.js
@@ -1,49 +1,13 @@
-let newPlants = [];
-let removePlants = [];
+let addPlantsInput = [];
+let removeAssociationIds = [];
 
-function addPlant() {
-    const plantSelect = document.getElementById('newPlantSelect');
-    const plantCount = document.getElementById('newPlantCount').value;
-    const plantId = plantSelect.value;
-    const plantName = plantSelect.options[plantSelect.selectedIndex].text;
 
-    const row = document.createElement('tr');
-    row.innerHTML = `
-                <td>${plantName}</td>
-                <td>...</td> <!-- Temperature -->
-                <td>...</td> <!-- Soil Humidity -->
-                <td>...</td> <!-- Air Humidity -->
-                <td>...</td> <!-- Light -->
-                <td>...</td> <!-- O2 -->
-                <td>New</td> <!-- Start Date -->
-                <td><input type="number" name="count_new_${plantId}" value="${plantCount}" min="1"></td>
-                <td><button type="button" onclick="removeNewPlant(this, '${plantId}')">Remove</button></td>
-            `;
-    document.getElementById('currentPlants').appendChild(row);
-    newPlants.push(plantId + ':' + plantCount);
-    updateHiddenInputs();
+function createElementFromHTML(htmlString){
+    const div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+    return div.firstChild;
 }
 
-function removePlant(associationId) {
-    const row = document.querySelector(`tr[data-association-id="${associationId}"]`);
-    if (row) {
-        row.remove();
-        removePlants.push(associationId);
-        updateHiddenInputs();
-    }
-}
-
-function removeNewPlant(button, plantId) {
-    const row = button.closest('tr');
-    row.remove();
-    newPlants = newPlants.filter(np => !np.startsWith(plantId + ':'));
-    updateHiddenInputs();
-}
-
-function updateHiddenInputs() {
-    document.getElementById('newPlantsInput').value = newPlants.join(',');
-    document.getElementById('removePlantsInput').value = removePlants.join(',');
-}
 
 const toDisplayDate = (localDate) => {
     console.log(localDate);
@@ -52,6 +16,101 @@ const toDisplayDate = (localDate) => {
     const year = String(localDate.getFullYear()).slice(-2); // Get last two digits of the year
 
     return `${day}/${month}/${year}`;
+}
+
+function removePlant(associationId) {
+    const row = document.querySelector(`tr[data-association-id="${associationId}"]`);
+    if (row) {
+        row.remove();
+        removeAssociationIds.push(associationId);
+        updateHiddenInputs();
+    }
+}
+
+function removeNewPlant(plantId){
+    const element = document.querySelector(`ul.currentPlants li[data-plant-id="${plantId}"]`);
+    addPlantsInput = addPlantsInput.filter(np => !np.startsWith(plantId + ':'));
+    updateHiddenInputs();
+}
+
+
+function updateHiddenInputs() {
+    document.getElementById('addPlantsInput').value = addPlantsInput.join(',');
+    document.getElementById('removeAssociationsInput').value = removeAssociationIds.join(',');
+}
+
+
+const addPlant = (plant_id) => {
+    const count = document.getElementById('newPlantCount').value;
+    const currentPlants = document.getElementById('currentPlants');
+    const [plant_name, temperature, soil_humidity, air_humidity, light, o2] = window.available_plants[plant_id];
+    const start_date_local = new Date();
+
+    const element = `
+            <li class="association" data-plant-id="${plant_id}">
+                <div class="controls">
+                    <p>
+                        <span><span>×</span>${count}</span>&nbsp;
+                        ${plant_name}&nbsp;
+                        <span>(${toDisplayDate(start_date_local)})</span>
+                    </p>
+    
+                    <button type="button" onclick="removeNewPlant(${plant_id})">Retirer</button>
+                </div>
+                <div class="config">
+                    <p>
+                        Temp&#x202F;: ${temperature}°C,
+                        Sol HR&#x202F;: ${soil_humidity}%,
+                        Air HR&#x202F;: ${air_humidity}%,
+                        Lux&#x202F;: ${light},
+                        O2&#x202F;: ${o2}%
+                    </p>
+                </div>
+            </li>`
+
+    currentPlants.innerHTML += element;
+
+}
+
+const updateSearchPlants = (searchValue) => {
+    const plantList = document.getElementById('plant-results');
+    const filteredPlants = Object.keys(window.available_plants).filter((id) => {
+        return window.available_plants[id][0].toLowerCase().includes(searchValue)
+    });
+
+    plantList.innerHTML = '';
+    filteredPlants.forEach((id) => {
+        const [plant_name, temperature, soil_humidity, air_humidity, light, o2] = window.available_plants[id];
+        const element = createElementFromHTML(`
+            <li data-plant-id="${id}">
+                <div role="button">
+                    <p>${plant_name}</p>
+                </div>
+                <div>
+                    <p>
+                        Temp&#x202F;: ${temperature}°C,
+                        Sol HR&#x202F;: ${soil_humidity}%,
+                        Air HR&#x202F;: ${air_humidity}%,
+                        Lux&#x202F;: ${light},
+                        O2&#x202F;: ${o2}%
+                    </p>
+                </div>
+            </li>`)
+
+        element.addEventListener('click', function(){
+            const plantInput = document.getElementById('newPlantInput');
+            plantInput.value = plant_name;
+            plantInput.setAttribute('data-plant-id', id);
+            plantList.innerHTML = '';
+        });
+
+        plantList.appendChild(element);
+    });
+
+    if(filteredPlants.length === 0){
+        plantList.innerHTML = '<li><p>Aucune plante trouvée</p></li>';
+    }
+
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -69,14 +128,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
         const element = `
-            <div class="association" data-association-id="${association_id}">
+            <li class="association" data-association-id="${association_id}">
                 <div class="controls">
-                    <p>${plant_name} <span>(${toDisplayDate(start_date_local)})</span></p>
+                    <p>
+                        <span><span>×</span>${count}</span>&nbsp;
+                        ${plant_name}&nbsp;
+                        <span>(${toDisplayDate(start_date_local)})</span></p>
     
-                    <div>
-                        <input type="number" name="count_${association_id}" value="${count}" min="0">
-                        <button type="button" onclick="removePlant(${association_id})">Retirer</button>
-                    </div>
+                    <button type="button" onclick="removePlant(${association_id})">Retirer</button>
                 </div>
                 <div class="config">
                     <p>
@@ -87,10 +146,16 @@ document.addEventListener("DOMContentLoaded", function() {
                         O2&#x202F;: ${o2}%
                     </p>
                 </div>
-            </div>`
+            </li>`
 
         currentPlants.innerHTML += element;
     })
+
+    document.getElementById('newPlantSearch').addEventListener('input', function(){
+        const searchValue = this.value.toLowerCase();
+        updateSearchPlants(searchValue);
+    })
+    updateSearchPlants('');
 
 
     // const plantInput = document.getElementById('newPlantInput');
