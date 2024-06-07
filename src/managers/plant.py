798df5729defc13211f92840b7599bf07fa344b7
Clement <clement.dupic@insa-lyon.fr>
2024-06-07 16:16:34 +0200

    bd changed default user role = guest and unable to change plants if guest

--- src/managers/plant.py
+++ src/managers/plant.py
@@ -1,31 +1,34 @@
 from flask import session, request, redirect, url_for
 
-from src.database.greenhouse import switch_greenhouse_custom_config
+from src.database.greenhouse import switch_greenhouse_custom_config, get_role_user
 from src.database.plant import (actualiaze_greenhouse_targets,
                                 add_association_plant, terminate_association)
 
 
 def plant_manager():
-    print(request.form)
-    if request.form.get('action') == 'custom':
+    if get_role_user(request.form.get('ghs'), session['user_name']) != 'guest':
+        if request.form.get('action') == 'custom':
 
-        switch_greenhouse_custom_config(request.form.get('ghs'))
-        session['success'] = "Les modifications ont bien été prises en compte."
+            switch_greenhouse_custom_config(request.form.get('ghs'))
+            session['success'] = "Les modifications ont bien été prises en compte."
 
-    elif request.form.get('action') == 'plant':
+        elif request.form.get('action') == 'plant':
 
-        add_plant_list = request.form.get('add-plant').split(',')
-        add_count_list = request.form.get('add-plant-count').split(',')
-        remove_associations_list = request.form.get('remove-associations').split(',')
-        remove_associations_count_list = request.form.get('remove-associations-count').split(',')
+            add_plant_list = request.form.get('add-plant').split(',')
+            add_count_list = request.form.get('add-plant-count').split(',')
+            remove_associations_list = request.form.get('remove-associations').split(',')
+            remove_associations_count_list = request.form.get('remove-associations-count').split(',')
 
-        add_association_plant(request.form.get('ghs'), [add_plant_list, add_count_list])
-        terminate_association(remove_associations_list, remove_associations_count_list)
-        actualiaze_greenhouse_targets(request.form.get('ghs'))
+            add_association_plant(request.form.get('ghs'), [add_plant_list, add_count_list])
+            terminate_association(remove_associations_list, remove_associations_count_list)
+            actualiaze_greenhouse_targets(request.form.get('ghs'))
 
-        session['success'] = "Les modifications ont bien été prises en compte."
+            session['success'] = "Les modifications ont bien été prises en compte."
+
+        else:
+            session['error'] = "Une erreur s'est produite lors de la modification de la configuration de la serre."
 
     else:
-        session['error'] = "Une erreur s'est produite lors de la modification de la configuration de la serre."
+        session['error'] = "Vous n'avez pas les permissions nécessaires pour effectuer cette action."
 
     return redirect(url_for('greenhouse_plants_page', greenhouse_serial=request.form.get('ghs')))
