b96d96b13055e5df4c603b8e81bffcc98c8b1190
Clement <clement.dupic@insa-lyon.fr>
2024-06-30 15:24:20 +0200

    adding mygrowbox.fr website code

--- SQL_Script/script.sql
+++ SQL_Script/script.sql
@@ -1,3 +1,4 @@
-select *
-from Sensors
-where type in ('temperature', 'soil_humidity', 'air_humidity', 'light', 'O2')
+INSERT INTO GreenHousePlants (plant_id, greenhouse_serial, count) VALUES (1, 'GrowBox-1', 2);
+INSERT INTO GreenHousePlants (plant_id, greenhouse_serial, count) VALUES (2, 'GrowBox-1', 2);
+INSERT INTO GreenHousePlants (plant_id, greenhouse_serial, count) VALUES (3, 'GrowBox-1', 1);
+INSERT INTO GreenHousePlants (plant_id, greenhouse_serial, count) VALUES (96, 'GrowBox-1', 20);
